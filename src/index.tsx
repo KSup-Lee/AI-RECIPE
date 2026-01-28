c0c326f914feb66e1e4559534ca622ec7581b972
KSup-Lee <ksl2486@gmail.com>
2026-01-28 12:13:22 +0900

    Update index.tsx

--- src/index.tsx
+++ src/index.tsx
@@ -1,13 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
+import './index.css'; // 방금 만든 CSS 파일을 여기서 불러옵니다!
 
 const rootElement = document.getElementById('root');
+
 if (!rootElement) {
-  throw new Error("Could not find root element to mount to");
+  throw new Error('Failed to find the root element');
 }
 
 const root = ReactDOM.createRoot(rootElement);
+
 root.render(
   <React.StrictMode>
     <App />
